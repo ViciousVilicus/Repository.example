8b04feb9a90322fd08ffbdb3e1210d91a89cf58e
VV <sus@ex.pl>
2022-05-13 08:40:02 +0200

    comment

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author uczen
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char znak= 'g';
+        int n=6;
+        double d=2.5;
+        System.out.print("hanime"+znak);
+        System.out.println("vanilla,1boy,1girl");
+    }
+    
+}
